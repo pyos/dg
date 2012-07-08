67835cc784688ee5725cc17bfa4017d60148fdf8
pyos <pyos100500@gmail.com>
2012-07-08 20:40:59 +0400

    Make bytecode generation core a little bit neater.

--- compile/codegen.py
+++ compile/codegen.py
@@ -3,20 +3,16 @@ import math
 import types
 import struct
 import functools
-import itertools
 
 from .. import const
 
+EXTENDED_ARG = functools.partial(struct.pack, '=BH', dis.opmap['EXTENDED_ARG'])
 
-iindex  = lambda it, v: next(i for i, q in enumerate(it) if q == v)
+nsplit  = lambda x, q: nsplit(x // q, q) + [x % q] if x >= q else [x]
 delay   = lambda f, g=None: type('delay', (), {'__int__': f, '__call__': g})()
 codelen = lambda cs: sum(
-    1 + (  # opcode length
-        c >= dis.HAVE_ARGUMENT and (  # 0 if no argument
-            2 +  # argument length
-            3 * int(math.log(int(v) or 1, 0x10000)) # EXTENDED_ARGs
-        )
-    ) for c, v in cs
+    1 + (c >= dis.HAVE_ARGUMENT and (3 * len(nsplit(int(v), 0x10000)) - 1))
+    for c, v in cs
 )
 
 
@@ -26,38 +22,42 @@ class MutableCode:
 
         super().__init__()
 
-        self.argc = len(args)
+        self.argc   = len(args)
         self.kwargc = len(kwargs)
 
-        self.names = []
-        self.consts = []
+        self.names    = []
+        self.consts   = []
         self.freevars = []
         self.cellvars = []
-        self.varnames = list(itertools.chain(args, kwargs, varargs, varkwargs))
-
-        self.cellnames = set(cell.varnames) | cell.cellnames if cell else set()
-        self.slowlocals = not isfunc
+        self.varnames = list(args) + list(kwargs) + list(varargs) + list(varkwargs)
 
         self.flags  = const.CO.OPTIMIZED | const.CO.NEWLOCALS if isfunc else 0
         self.flags |= const.CO.VARARGS   if varargs   else 0
         self.flags |= const.CO.VARKWARGS if varkwargs else 0
-        self.bytecode = []
-
-        self.stacksize = self.cstacksize = 0
+        self.bytecode   = []
+        self.stacksize  = 0
+        self.cstacksize = 0
 
         self.filename = '<generated>'
         self.lineno   = 1
         self.lnotab   = {}
 
-    def __getattr__(self, name):
+        # Names of variables that could be added to `freevars`.
+        self.cellnames = set(cell.varnames) | cell.cellnames if cell else set()
 
-        if name.isupper():
+        # Whether to use the `f_locals` hashmap instead of fast locals.
+        # Enabled for global NS and functions that do STORE_LOCALS.
+        self.slowlocals = not isfunc
 
-            return functools.partial(self.append, name)
-
-        raise AttributeError(name)
+  ### LNOTAB
 
     def mark(self, e):
+
+        '''Add an entry to the line number table.
+
+        :param e: the statement that will generate the next instruction.
+
+        '''
 
         self.lnotab[len(self.bytecode)] = e.reparse_location.start[1]
 
@@ -77,25 +77,33 @@ class MutableCode:
                 yield bytes((bytediff % 256, linediff % 256))
                 offset, lineno = offset_, lineno_
 
-    def use(self, v, id, *containers):
+  ### OPCODE ARGUMENTS
 
-        if not isinstance(v, str) and self.consts not in containers:
+    def name(self, v, container):
 
+        if not isinstance(v, str) and container is not self.consts:
+
+            # FIXME that should be done in `compile.__init__`, not here.
             raise Exception(const.ERR.NONCONST_ATTR)
 
-        for i, ct in enumerate(containers):
+        v in container or container.append(v)
+        return container.index(v)
 
-            i < id and v in ct and ct.remove(v)
+    def freevar(self, v):
 
-            if i >= id and v in ct:
+        if v not in self.freevars:
+        
+            if v in self.cellnames:
 
-                break
+                self.freevars.append(v)
 
-        else:
+            elif v not in self.cellvars:
 
-            containers[id].append(v)
+                self.cellvars.append(v)
 
-        return delay(lambda _: iindex(itertools.chain.from_iterable(containers), v))
+        # Free and cell variables use the same index space, so we
+        # don't know their indices right now.
+        return delay(lambda _: (self.cellvars + self.freevars).index(v))
 
     def jump(self, absolute, reverse=False):
 
@@ -104,20 +112,15 @@ class MutableCode:
         def finish(v):
 
             v.end = len(self.bytecode)
-            hasattr(v, '_c') and delattr(v, '_c')  # Empty the value cache.
+            # Empty the value cache.
+            hasattr(v, '_c') and delattr(v, '_c')
 
-        # Returns 0 if recurring and the actual jump target otherwise.
-        # Will fail given the length of bytecode is big enough
-        # (i.e. the target for this jump >= 0x10000)
-        #
-        # FIXME given the size constraints of `int`, the value may increase
-        #       by 3 (possibly 6). Repeating the calculation
-        #       2 more times may fix the issue.
-        #
         def to_int(v):
 
             if not hasattr(v, '_c'):
 
+                # codelen() may call this function again,
+                # so we need to prevent the infinite recursion.
                 v._c = 0
                 v._c = codelen(
                     self.bytecode[0:start] if reverse else
@@ -128,65 +131,92 @@ class MutableCode:
 
         return delay(to_int, finish)
 
+  ### OPCODE MANIPULATION
+
+    def __getattr__(self, name):
+
+        if name.isupper():
+
+            # `MutableCode.SOME_OPCODE_NAME(...)` is an alias to
+            # `MutableCode.append('SOME_OPCODE_NAME', ...)`
+            return functools.partial(self.append, name)
+
+        raise AttributeError(name)
+
     def append(self, name, value=0, delta=0):
 
+        '''Append a new opcode to the bytecode sequence.
+
+        :param name: name of the bytecode (should be in `dis.opmap`.)
+
+        :param value: opcode-dependent.
+
+          argumentless opcodes: ignored
+          relative jumps:: ignored
+          absolute jumps:: the jump is in reverse direction when negative
+          name ops::       name of the variable to use
+          LOAD_CONST::     the value to load
+          COMPARE_OP::     the operator name
+          all others::     passed as is (must be an integer)
+
+        :param delta: how much items will this opcode push onto the stack.
+
+        :return: the argument to that opcode.
+
+        '''
+
+        self.flags      |= name == 'YIELD_VALUE' and const.CO.GENERATOR
+        self.slowlocals |= name == 'STORE_LOCALS'
+
         code = dis.opmap[name]
-
-        if code == dis.opmap['YIELD_VALUE']:
-
-            self.flags |= const.CO.GENERATOR
-
-        if code == dis.opmap['STORE_LOCALS']:
-
-            self.slowlocals = True
 
         if code in dis.hasjabs and value < 0:
 
             # Reverse jump.
             # Note that relative jumps can't be in reverse direction.
             jmp = self.jump(absolute=True, reverse=True)
-            return lambda: (jmp(), self.bytecode.append((code, jmp)))
+            # No need to call `jmp`, it will ignore that anyway.
+            return lambda: self.bytecode.append((code, jmp))
 
         self.bytecode.append((
             code,
-            0                                 if code <  dis.HAVE_ARGUMENT else
-            self.jump(absolute=False)         if code in dis.hasjrel       else
-            self.jump(absolute=True)          if code in dis.hasjabs       else
-            self.use(value, 0, self.names)    if code in dis.hasname       else
-            self.use(value, 0, self.varnames) if code in dis.haslocal      else
-            self.use(value, 0, self.consts)   if code in dis.hasconst      else
-            self.use(value, value in self.cellnames, self.cellvars, self.freevars)
-                if code in dis.hasfree else
-            dis.cmp_op.index(value) if code in dis.hascompare else
+            self.jump(absolute=False)       if code in dis.hasjrel    else
+            self.jump(absolute=True)        if code in dis.hasjabs    else
+            self.name(value, self.names)    if code in dis.hasname    else
+            self.name(value, self.varnames) if code in dis.haslocal   else
+            self.name(value, self.consts)   if code in dis.hasconst   else
+            self.freevar(value)             if code in dis.hasfree    else
+            dis.cmp_op.index(value)         if code in dis.hascompare else
             value
         ))
 
+        self.stacksize  += max(0, delta)
         self.cstacksize += delta
-        self.stacksize = max(self.stacksize, self.cstacksize)
         return self.bytecode[-1][1]
 
     def make_bytecode(self):
 
         for code, value in self.bytecode:
 
-            value = int(value)
-            oparg = [value % 0x10000]
-
-            while value > 0x10000:
-
-                value //= 0x10000
-                oparg.insert(0, value % 0x10000)
-
-            for arg in oparg[:-1]:
-
-                yield struct.pack('=BH', dis.opmap['EXTENDED_ARG'], arg)
-
+            oparg = nsplit(int(value), 0x10000)
+          # yield from map(EXTENDED_ARG, oparg[:-1])
+            yield b''.join(map(EXTENDED_ARG, oparg[:-1]))
             yield struct.pack(*
                 ('=BH', code, oparg[-1]) if code >= dis.HAVE_ARGUMENT else
                 ('=B',  code)
             )
 
+  ### MAIN
+
     def compile(self, name='<lambda>'):
+
+        '''Compile the mutable code into an immutable code.
+
+        :param name: name of the function/module/other stuff this code implements.
+
+        :return: a Python code object.
+
+        '''
 
         return types.CodeType(
             self.argc,
@@ -195,8 +225,7 @@ class MutableCode:
             self.stacksize,
             self.flags | (
                 const.CO.NESTED if self.freevars else
-                0               if self.cellvars else
-                const.CO.NOFREE
+                const.CO.NOFREE if not self.cellvars else 0
             ),
             b''.join(self.make_bytecode()),
             tuple(self.consts),
