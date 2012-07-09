6e48e5a59d5bdaa1d4ceb4c6abf6ba4c37d0937c
pyos <pyos100500@gmail.com>
2012-07-09 20:02:34 +0400

    Add some command-line arguments.
    -p/--print -- print the result of the last expression in the top level
    file       -- the file to parse (default: -)
    arguments  -- additional arguments (i.e. sys.argv[1:])

--- __main__.py
+++ __main__.py
@@ -1,4 +1,5 @@
 import sys
+import argparse
 
 from . import parse
 from . import compile
@@ -8,17 +9,44 @@ from .interactive import Interactive
 
 class Interactive (Interactive):
 
+    def __init__(self, args):
+
+        super().__init__()
+
+        self.args   = args
+        self.single = sys.stdin.isatty() or args.print
+
+    def displayhook(self, value):
+
+        self.args.print and print(value)
+        return super().displayhook(value)
+
+    def traceback(self, trace):
+
+        # When running in non-interactive mode, strip the first 4 lines.
+        # These correspond to stuff in this module.
+        return super().traceback(trace)[4 * (not sys.stdin.isatty()):]
+
     def compile(self, code):
 
         q = parse.r.compile_command(code)
-        q = q if q is None else compile.r(q, name='<module>', single=True)
+        q = q if q is None else compile.r(q, name='<module>', single=self.single)
         return q
 
     def run(self, ns):
 
-        q = parse.r(sys.stdin.read(), '<stdin>')
-        q = compile.r(q, name='<module>')
+        q = parse.r(sys.stdin.read(), sys.stdin.name)
+        q = compile.r(q, name='<module>', single=self.single)
         return self.eval(q, ns)
 
 
-Interactive().shell(__name__)
+parser = argparse.ArgumentParser()
+parser.add_argument('-p', '--print', action='store_true', help='when compiling, make the top-level code print its evaluation result in addition to returning it (does not affect REPL)')
+parser.add_argument('file', nargs='?', help='files to parse/compile', type=argparse.FileType())
+parser.add_argument('arguments', nargs='*', help='additional arguments')
+args = parser.parse_args()
+
+sys.argv = [args.file.name if args.file else '-'] + args.arguments
+sys.stdin = args.file or sys.stdin
+
+Interactive(args).shell(__name__)
