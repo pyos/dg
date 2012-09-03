99351f146b4cf1d4ef180e6e9333baa02d6c46f0
pyos <pyos100500@gmail.com>
2012-09-03 18:35:03 +0400

    Add two more aliases to `call`.

--- compile/core.py
+++ compile/core.py
@@ -280,6 +280,9 @@ class Compiler:
       , '.':  getattr
       , '\n': chain
       , '->': function
+      # ...
+      , 'new':  call  # yes, this IS the same in Python
+      , 'call': call
     }
 
     fake_attrs = {}
