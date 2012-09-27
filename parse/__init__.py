3f02e5f386bf22cff422ec1617b20c7537e0ea00
pyos <pyos100500@gmail.com>
2012-09-27 13:11:10 +0400

    Allow link names to contain semicolons.

--- parse/__init__.py
+++ parse/__init__.py
@@ -336,7 +336,7 @@ def string(stream, token):
     yield tree.Constant(ast.literal_eval('{1}{0}{3}{0}'.format(g, *token.groups())))
 
 
-@ParserState.token(r"(\w+'*|\*+(?=:)|([!$%&*+\--/:<-@\\^|~]+|,+))|\s*(\n)|`(\w+'*)`")
+@ParserState.token(r"(\w+'*|\*+(?=:)|([!$%&*+\--/:<-@\\^|~;]+|,+))|\s*(\n)|`(\w+'*)`")
 #
 # link = word | < ascii punctuation > + | ',' + | ( '`', word, '`' ) | '\n'
 #
