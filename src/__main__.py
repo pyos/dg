f4f572343c5364831c967fef795f6fae8a0533f2
Doug <pyos100500@gmail.com>
2012-11-20 16:09:41 +0000

    Force UNIX-style line breaks in index.html.

--- src/__main__.py
+++ src/__main__.py
@@ -13,5 +13,5 @@ data    = {name_of(x): dmark.parse(open(x).read()) for x in files}
 for f in glob.glob(os.path.join(__path__[0], '*.html')):
 
     src = open(f)
-    tgt = open(os.path.join(os.path.dirname(f), os.pardir, os.path.basename(f)), 'w')
+    tgt = open(os.path.join(os.path.dirname(f), os.pardir, os.path.basename(f)), 'w', newline='\n')
     tgt.write(src.read().format(**data))
