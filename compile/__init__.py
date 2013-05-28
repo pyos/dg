2a96466e7ab418e9707f4dd390a199842bf7f3b9
pyos <pyos100500@gmail.com>
2013-05-28 20:02:52 +0400

    Rename `compile.it` to `compile.ast`.
    `compile.it` now compiles a string instead of a `StructMixIn`.

--- compile/__init__.py
+++ compile/__init__.py
@@ -4,11 +4,16 @@ from .  import core, bootstrap
 
 def it(st):
 
-    c = core.CodeGenerator('<module>')
-    c.loadop('RETURN_VALUE', st, delta=0)
-    return c.compiled
+    return ast(parse.it(st))
 
 
 def fd(fd, name='<stream>'):
 
-    return it(parse.fd(fd, name))
+    return ast(parse.fd(fd, name))
+
+
+def ast(st):
+
+    c = core.CodeGenerator('<module>')
+    c.loadop('RETURN_VALUE', st, delta=0)
+    return c.compiled
