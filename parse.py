676ffa525f343ea56a0d569b0c21279005de574e
pyos <pyos100500@gmail.com>
2013-07-13 07:48:30 +0400

    Lower the left binding strength of `->` and `~>`.
    Parentheses around function arguments are now optional.

--- parse.py
+++ parse.py
@@ -126,8 +126,8 @@ has_priority = (lambda f: lambda a, b: f(a)[0] > f(b)[1])(lambda m, g={
     'and':   (-140, -140),  # B if A else A
     'or':    (-150, -150),  # A if A else B
     '$':     (-150, -160),  # call with one argument and no f-ing parentheses
-    '->':    (-5,   -180),  # a function
-    '~>':    (-5,   -180),  # a method (i.e. a function with `self` as an argument)
+    '->':    (-25,  -180),  # a function
+    '~>':    (-25,  -180),  # a method (i.e. a function with `self` as an argument)
     ',':     (-170, -170),  # a tuple
     '=':     (-170, -180),  # assignment
     '!!=':   (-170, -180),  # in-place versions of some of the other functions
