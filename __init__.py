ef1332e449c8faeeb85c855534fdde3c28cd72aa
pyos <pyos100500@gmail.com>
2013-08-07 22:45:00 +0400

    Rename `tag` to `PY_TAG`.

--- __init__.py
+++ __init__.py
@@ -4,26 +4,22 @@ import os.path
 
 
 if not hasattr(sys, 'implementation'):
-    raise ImportError('Python 3.3 or newer is required.')
+    raise ImportError('Python 3.3 or newer is required')
 
-tag = sys.implementation.cache_tag
+PY_TAG = sys.implementation.cache_tag
 
-if tag is None:
+if PY_TAG is None:
     # Never seen this to be true, but Python documentation
     # mentions that it's possible.
-    raise ImportError('Module caching is disabled. Cannot load the bundle.')
+    raise ImportError('cannot load the bundle since module caching is disabled')
 
-bundle = os.path.join(__path__[0], 'bundles', tag + '.dgbundle')
-
-if not os.path.isfile(bundle):
-    # Probably unsupported.
-    raise ImportError('No bundle found for `{}`.'.format(tag))
+__file__ = os.path.join(__path__[0], 'bundles', PY_TAG + '.dgbundle')
 
 try:
-    with open(bundle, 'rb') as _fd:
+    with open(__file__, 'rb') as _fd:
         for _c in marshal.load(_fd):
             eval(_c)
 except IOError:
-    raise ImportError('The bundle for `{}` is inaccessible.'.format(tag))
+    raise ImportError('`{}.dgbundle` is inaccessible'.format(PY_TAG))
 except Exception:
-    raise ImportError('The bundle for `{}` is corrupt.'.format(tag))
+    raise ImportError('`{}.dgbundle` is corrupt'.format(PY_TAG))
