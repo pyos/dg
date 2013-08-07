d2aba134bb0a40534b00d74fdb10aa00e76a8ddb
pyos <pyos100500@gmail.com>
2013-08-07 17:00:10 +0400

    Rewrite __init__.

--- __init__.py
+++ __init__.py
@@ -4,23 +4,33 @@ import os.path
 
 
 if not hasattr(sys, 'implementation'):
-    raise ImportError('Python version is too old. 3.3. is required.')
+    if sys.hexversion >= 0x03020000:
+        # 3.2 is supported, although it lacks `sys.implementation`.
+        # We'll use `platform` instead.
+        import platform
 
-_tag = sys.implementation.cache_tag
+        tag = platform.python_implementation().lower() + '-32'
+    else:
+        raise ImportError('Python 3.2 or newer is required.')
+else:
+    tag = sys.implementation.cache_tag
 
-if _tag is None:
-    raise ImportError('Module caching is disabled. Failed to locate the bundle.')
+if tag is None:
+    # Never seen this to be true, but Python documentation
+    # mentions that it's possible.
+    raise ImportError('Module caching is disabled. Cannot load the bundle.')
 
-_bundle = os.path.join(__path__[0], 'bundles', _tag + '.dgbundle')
+bundle = os.path.join(__path__[0], 'bundles', tag + '.dgbundle')
 
-if not os.path.isfile(_bundle):
-    raise ImportError('No bundle found for this interpreter (tag: {}).'.format(tag))
+if not os.path.isfile(bundle):
+    # Probably unsupported.
+    raise ImportError('No bundle found for `{}`.'.format(tag))
 
-with open(_bundle, 'rb') as _fd:
-    try:
-        _codes = marshal.load(_fd)
-    except Exception as e:
-        raise ImportError('Corrupt bundle (tag: {}).'.format(tag))
-
-
-for _c in _codes: eval(_c)
+try:
+    with open(bundle, 'rb') as _fd:
+        for _c in marshal.load(_fd):
+            eval(_c)
+except IOError:
+    raise ImportError('The bundle for `{}` is inaccessible.'.format(tag))
+except Exception:
+    raise ImportError('The bundle for `{}` is corrupt.'.format(tag))
