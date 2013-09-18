4512f9a4b30257421ad00fe60e494e9dff08ec0d
pyos <pyos100500@gmail.com>
2013-09-18 11:38:34 +0400

    Add a distutils script.
    I hope pip will be able to install dg now.

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from distutils.core import setup
+
+setup(
+    name='dg',
+    version='HEAD',
+    description='A programming language for the CPython VM',
+    author='pyos',
+    author_email='pyos100500@gmail.com',
+    url='https://github.com/pyos/dg.git',
+    packages=['dg'],
+    package_dir={'dg': '.'},
+    package_data={'dg': ['*.dg', 'core/*', 'bundles/*', 'contrib/*']}
+)
