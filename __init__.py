0de910985842f9e650fde53c601761dfce03eb2f
pyos <pyos100500@gmail.com>
2014-03-10 01:22:01 +0400

    Slightly improve ImportError messages.

--- __init__.py
+++ __init__.py
@@ -1,21 +1,20 @@
+import os
 import sys
 import marshal
-import os.path
 
 
-if not hasattr(sys, 'implementation'):
-    raise ImportError('Python 3.3 or newer is required')
+if sys.hexversion < 0x03030000:
+    raise ImportError('python >= 3.3 required')
 
 if sys.implementation.cache_tag is None:
-    raise ImportError('cannot load the bundle since module caching is disabled')
+    raise ImportError('python implementation does not use bytecode')
 
 PY_TAG      = sys.implementation.cache_tag
 PY_VERSION  = sys.hexversion
-BUNDLE_DIR  = os.path.join(__path__[0], 'bundle')
-BUNDLE_FILE = os.path.join(BUNDLE_DIR, PY_TAG + '.dgbundle')
+BUNDLE_FILE = os.path.join(__path__[0], 'bundle', PY_TAG + '.dgbundle')
 
 if not os.path.exists(BUNDLE_FILE):
-    raise ImportError('unsupported platform: {}'.format(PY_TAG))
+    raise ImportError('python implementation {!r} not supported'.format(PY_TAG))
 
 with open(BUNDLE_FILE, 'rb') as _fd:
     for _c in marshal.load(_fd):
