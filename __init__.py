ae64fd28294aa4d64420dbb446753e6b9cbeec04
pyos <pyos100500@gmail.com>
2015-02-22 04:00:13 +0300

    Replace `__builtins__` instead of dumping stuff into globals.
    Can't believe I hadn't thought of that.

--- __init__.py
+++ __init__.py
@@ -1,25 +1,21 @@
 import os
 import sys
-import marshal
 
+try:
+    PY_TAG     = sys.implementation.cache_tag or 'unknown'
+    PY_VERSION = sys.hexversion
+    BUNDLE_DIR = os.path.join(__path__[0], 'bundle')
+except AttributeError:
+    raise ImportError('Python >= 3.4 required')
 
-if not hasattr(sys, 'implementation'):
-    raise ImportError('python >= 3.3 required')
+def load():
+    from marshal import load
 
-if sys.implementation.cache_tag is None:
-    raise ImportError('python implementation does not use bytecode')
+    try:
+        with open(os.path.join(BUNDLE_DIR, PY_TAG + '.dgbundle'), 'rb') as fd:
+            for code in load(fd):
+                eval(code)
+    except FileNotFoundError:
+        raise ImportError('python implementation {!r} not supported'.format(PY_TAG))
 
-PY_TAG      = sys.implementation.cache_tag
-PY_VERSION  = sys.hexversion
-BUNDLE_DIR  = os.path.join(__path__[0], 'bundle')
-BUNDLE_FILE = os.path.join(BUNDLE_DIR, PY_TAG + '.dgbundle')
-
-if not os.path.exists(BUNDLE_FILE):
-    raise ImportError('python implementation {!r} not supported'.format(PY_TAG))
-
-with open(BUNDLE_FILE, 'rb') as _fd:
-    for _c in marshal.load(_fd):
-        eval(_c)
-
-del _c
-del _fd
+load()
