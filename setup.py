5a91b3b9d2091064b3419e41fd7d07c3e89828c6
pyos <pyos100500@gmail.com>
2015-05-14 22:48:51 +0300

    Bump version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='dg',
-    version='1.0.0+git',
+    version='1.1.0+git',
     description='A programming language for the CPython VM',
     author='pyos',
     author_email='pyos100500@gmail.com',
